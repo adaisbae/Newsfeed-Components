227ae9da9a608b6e28d10cb0a678bf91c067ca9e
adaisbae <melissajoneswrites@gmail.com>
2019-06-19 17:32:24 -0400

    was finally able to get expandArticle to work. need to add logic to close articles

--- Article/Article.js
+++ Article/Article.js
@@ -14,15 +14,17 @@ class Article {
     this.expandButton.textContent = 'Expand';
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.article.addEventListener('click', this.expandArticle())
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.article.classList.toggle('article-open');
+    // if (this.article.classList === 'article-open'){
+    //   this.article.classList.toggle('close')
+    // }
   }
 }
-
 /* START HERE: 
 
 - Select all classes named ".article" and assign that value to the articles variable.  
